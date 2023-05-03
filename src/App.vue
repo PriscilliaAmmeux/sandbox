bcb5e5a1df7d9174c60772c844b2085ce3029c87
Priscillia Ammeux <priscillia.ammeux@gmail.com>
2023-05-03 11:43:41 +0200

    add component and style

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
 <script setup>
+import watchers from "./components/watchers.vue";
+import declarativeRendering from "./components/declarativeRendering.vue";
 import listRendering from "./components/listRendering.vue";
 import checkbox from "./components/checkbox.vue";
 import conditionalRenduring from "./components/conditionalRenduring.vue";
@@ -8,32 +10,59 @@ import attributeBindings from "./components/attributeBindings.vue";
 
 <template>
   <h1>My sandbox</h1>
-
-  <h2>Declative Rendering</h2>
-  <declarativeRendering />
-  <p>********************</p>
-
-  <h2>Attribute Bindings</h2>
-  <attributeBindings />
-  <p>********************</p>
-
-  <h2>Event listeners</h2>
-  <eventListeners />
-  <p>********************</p>
-
-  <h2>Conditional renduring</h2>
-  <conditionalRenduring />
-  <p>********************</p>
-
-  <h2>List rendering</h2>
-  <listRendering />
-  <p>********************</p>
-
-  <h2>Checkbox</h2>
-  <checkbox />
-  <p>********************</p>
-
-  <h2>Lifecycle and Template Refs</h2>
-  <lifeCycle />
-  <p>********************</p>
+  <div class="grid-container">
+    <div class="grid-item">
+      <h2>Declative Rendering</h2>
+      <declarativeRendering />
+    </div>
+    <div class="grid-item">
+      <h2>Attribute Bindings</h2>
+      <attributeBindings />
+    </div>
+    <div class="grid-item">
+      <h2>Event listeners</h2>
+      <eventListeners />
+    </div>
+    <div class="grid-item">
+      <h2>Conditional renduring</h2>
+      <conditionalRenduring />
+    </div>
+    <div class="grid-item">
+      <h2>List rendering</h2>
+      <listRendering />
+    </div>
+    <div class="grid-item">
+      <h2>Checkbox</h2>
+      <checkbox />
+    </div>
+    <div class="grid-item">
+      <h2>Watchers</h2>
+      <watchers />
+    </div>
+  </div>
 </template>
+
+<style>
+body {
+  font-family: Arial, sans-serif;
+  background-color: #f0f0f0;
+}
+
+h1 {
+  color: #333;
+  text-align: center;
+  font-size: 2rem;
+}
+.grid-container {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 5px;
+}
+
+.grid-item {
+  background-color: #ccc;
+  text-align: center;
+  font-size: 20px;
+  padding: 5px;
+}
+</style>
